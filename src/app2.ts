41fc72e7a09dd40e217aba14fb6839e9fd53d843
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-02-22 22:11:05 +0100

    refactor: integrate transaction router and model into app initialization

--- src/app2.ts
+++ src/app2.ts
@@ -4,13 +4,17 @@ import "dotenv/config";
 import { IAccountModel } from "./features/account/types";
 import { createCategoryRouter } from "./features/category/router";
 import { ICategoryModel } from "./features/category/types";
+import { createTransactionRouter } from "./features/transaction/router";
+import { ITransactionModel } from "./features/transaction/types";
 // después
 export const createApp = ({
   accountModel,
   categoryModel,
+  transactionModel,
 }: {
   accountModel: IAccountModel;
   categoryModel: ICategoryModel;
+  transactionModel: ITransactionModel;
 }) => {
   const app = express();
   app.use(json());
@@ -18,6 +22,7 @@ export const createApp = ({
 
   app.use("/accounts", createAccountRouter({ accountModel }));
   app.use("/categories", createCategoryRouter({ categoryModel }));
+  app.use("/transactions", createTransactionRouter({ transactionModel }));
 
   const PORT = process.env.PORT ?? 8080;
 
