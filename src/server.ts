e277eee5c8406b729df58bb833dcd03853acfd93
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-03-14 21:17:48 +0100

    refactor: implement main application setup with middleware and routes

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@ import { AccountModel } from '@/features/account/model'
 import { CategoryModel } from '@/features/category/model'
 import { SummaryModel } from '@/features/summary/model'
 import { TransactionModel } from '@/features/transaction/model'
-import { createApp } from '@/app2'
+import { createApp } from '@/app'
 import { UserModel } from '@/features/user/model'
 
 createApp({
