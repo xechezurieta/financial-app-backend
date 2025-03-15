09c579a846f9fb76874e54887fc56b2e6085267a
Xabier Echezurieta <xabier.echezurieta@gmail.com>
2025-03-15 21:52:12 +0100

    refactor: add path alias for tests in tsconfig and vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -15,7 +15,8 @@ export default defineConfig({
 	},
 	resolve: {
 		alias: {
-			'@': fileURLToPath(new URL('./src', import.meta.url))
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+			'@tests': fileURLToPath(new URL('./tests', import.meta.url))
 		}
 	}
 })
